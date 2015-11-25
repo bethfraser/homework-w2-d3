fa4df180ddc088d52f6f55bf00716894659b5d52
Beth Fraser <bethanyfraser@live.co.uk>
2015-11-25 14:57:15 +0000

    Created methods to show if the game has been won.

--- game.rb
+++ game.rb
@@ -16,6 +16,16 @@ class Game
     # To decide which of the pieces to use, the current value of turn is divided by 2 and the remainder returned - this will always be 0 or 1, which corresponds to the two indexes of O and X. Turn is then increased by 1 so that on the next turn the remainder will be the other value (0 or 1).
     @turn += 1
     display_board
+    if has_won?(:o)
+      puts "O has won!"
+      self.reset
+      return
+    end
+    if has_won?(:x)
+      puts "X has won!"
+      self.reset
+      return
+    end
     if board_full?
       puts "All spaces are used! The board has now been reset."
       self.reset
@@ -33,12 +43,44 @@ class Game
     @pieces.reverse!
   end
 
+  def has_won?(symbol)
+    horizontal_line?(symbol, @board) || 
+    vertical_line?(symbol) || 
+    diagonal_line?(symbol)
+  end
+
+  def horizontal_line?(symbol, board)
+    board.any? { |row| row_has_winning_line(row, symbol) }
+  end
+
+  def row_has_winning_line(row, symbol)
+    row.all? { |space| space == symbol } 
+  end
+
+  def vertical_line?(symbol)
+    new_board = @board.transpose
+    horizontal_line?(symbol, new_board)
+  end
+
+  def diagonal_line?(symbol)
+    # return true if @board[0][0] == symbol && @board[0][0] == @board[1][1] && @board[0][0] == @board[2][2]
+    # return true if @board[0][2] == symbol && @board[0][2] == @board[1][1] && @board[0][2] == @board[2][0]
+    # return false
+    middle_piece = @board[1][1]
+    return false if middle_piece != symbol
+    top_left_bottom_right = @board[0][0] == symbol && @board[2][2] == symbol
+    top_right_bottom_left = @board[0][2] == symbol && @board[2][0] == symbol
+    top_left_bottom_right || top_right_bottom_left
+  end
+
+
   private 
+
+
 
   def board_full?
     @board.flatten.all?
   end
-
 
   def row_to_string(row)
     row_symbols = row.map do |piece|
--- game_runner.rb
+++ game_runner.rb
@@ -15,12 +15,22 @@ game.display_board
 game.reset
 puts "Game board has been reset\n"
 
+game.place_piece(0,0)
+game.place_piece(1,0)
+game.place_piece(0,1)
+game.place_piece(1,1)
+game.place_piece(0,2)
+
+
+puts game.has_won?(:o)
+
 
 
 #To Do
 # don't allow putting pieces out of range
 # rotate the starting piece when resetting
 # automatically reset when board is full and tell us it's a draw
+# Detect when someone has won and notify
 
 
 
